046f6d580f562096ea0de9a66a1a7f0307b4b65b
mle-gars <maxime.legars@hotmail.fr>
2024-03-19 11:49:01 +0100

    :new branche features1

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+class Infra :
+
+    def __init__(self, infra_id, infra_type, length):
+
+    
+
+class House :
+
+    def __init__(self, id_bat, nb_house):
+    
